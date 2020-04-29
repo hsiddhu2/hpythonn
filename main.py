ed4518e8be074f8a52b7c8d9587bce511f40dd97
hsiddhu2 <hpsiddhu@gmail.com>
2020-04-29 19:06:47 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,19 @@
+'''
+Follow @HPYTHONN for daily updates
+Turn On Post Notications On and 
+dont' miss any new updates ! ! !
+'''
+
+# Logical Operators 
+# PRECEDENCE - arithmetic > comparison > not > and/or
+
+x = 5 * 7 >= 3 + 5 * (7-1) and not False
+print(x)
+
+# A: 35     B: 0
+# C: True   D: False
+
+
+
+
+
